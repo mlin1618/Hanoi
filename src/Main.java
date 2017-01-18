d03c5b17fbff39f9719eec57443631d5e41efe88
ml996 <ml996@pvsd.org>
2017-01-18 13:43:38 -0500

    ..

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        System.out.println("Tower of Hanoi Game. \nThis program will solve this puzzle for you. \nPlease input the number of disks to solve for: ");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         TowersOfHanoi T = new TowersOfHanoi(number);
